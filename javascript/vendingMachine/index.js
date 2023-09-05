6438fcc6b69412a391a7ddf6b9d8e4864bf6dabc
maskelog <blitzmaske@gmail.com>
2023-09-05 14:15:56 +0900

    20230905

--- javascript/vendingMachine/index.js
+++ javascript/vendingMachine/index.js
@@ -1,13 +1,8 @@
 import ColaGenerator from "./js/colaGenerator.js";
+import VendingMachineFunc from "./js/vendingMachineFunc.js";
 
 const colaGenerator = new ColaGenerator();
-// console.log(await colaGenerator.loadData());
 colaGenerator.setup();
 
-// 탑레벨 await 최근 추가된 문법
-
-// (async () => {
-//   await colaGenerator.loadData()
-// })()
-
-// https://v8.dev/features/top-level-await
+const vendingMachine = new VendingMachineFunc();
+vendingMachine.setup();
--- javascript/vendingMachine/js/colaGenerator.js
+++ javascript/vendingMachine/js/colaGenerator.js
@@ -1,51 +1,49 @@
 class ColaGenerator {
-  constructor() {
-    this.itemList = document.querySelector('.cola-list');
-  }
 
-  // 모든것을 초기화 하는 함수
-  async setup() {
-      const response = await this.loadData();
-      this.colaFactory(response);
-  }
+    constructor() {
+        this.itemList = document.querySelector('.cola-list');
+    }
 
-  async loadData() {
-    try {
-      const response = await fetch('./items.json');
-      
-      if (!response.ok) {
-        throw new Error('HTTP ERROR!! :' + response.status);
-      }
+    // 콜라 객체를 초기화합니다.
+    async setup() {
+        const response = await this.loadData();
+        this.colaFactory(response);
+    }
 
-      return await response.json();
-    } catch (error) {}
-      console.error('콜라데이터를 로딩하는 중 문제가 발생했습니다 :' + error);
-  }
+    // 콜라 관련 데이터를 로드합니다.
+    async loadData() {
 
-  // <li>
-  // <!-- JS : 클래스 on 유/무 제어 -->
-  // <button type="button" class="btn-cola on">
-  //   <img src="./img/cola-original.png" alt="" />
-  //   <span class="cola-name">Original_Cola</span>
-  //   <strong class="cola-price">1000원</strong>
-  // </button>
-  // </li>
+        try {
+            const response = await fetch('./items.json');
 
-  colaFactory(data) {
-    const docFrag = new DocumentFragment();
-    data.forEach((el) => {
-      const item = document.createElement('li');
-      const itemTemplate = `  <button type="button" class="btn-cola on">
-      <img src="./img/cola-original.png" alt="" />
-      <span class="cola-name">Original_Cola</span>
-      <strong class="cola-price">1000원</strong>
-    </button>`;
-      item.innerHTML = itemTemplate;
-      this.itemList.append(item);
-    });
+            if (!response.ok) {
+                throw new Error('HTTP ERROR!! :' + response.status);
+            }
+            return await response.json();
+        } catch (error) {
+            console.error('콜라데이터를 로딩하는중에 문제가 발생했습니다. :' + error);
+        }
+    }
 
-    this.itemList.append(docFrag);
-  }
+    // 콜라의 템플릿 코드입니다.
+    colaFactory(data) {
+        const docFrag = new DocumentFragment();
+        data.forEach((el) => {
+            const item = document.createElement('li');
+            const itemTemplate = `
+            <button type="button" class="btn-cola" data-item="${el.name}" data-count="${el.count}" data-price="${el.cost}" data-img="${el.img}">
+                <img src="./img/${el.img}" alt="${el.name}" />
+                <span class="cola-name">${el.name}</span>
+                <strong class="cola-price">${el.cost}원</strong>
+            </button>
+            `;
+            item.innerHTML = itemTemplate;
+            docFrag.append(item);
+        });
+
+        this.itemList.append(docFrag);
+    }
 }
+
 
 export default ColaGenerator;
