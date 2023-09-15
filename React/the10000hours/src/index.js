ae5501c98bcc9b8714aae7aafbed5ea542c0c9a6
maskelog <blitzmaske@gmail.com>
2023-09-15 23:29:40 +0900

    20230915

--- React/the10000hours/src/index.js
+++ React/the10000hours/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 // import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import Quiz from "./Quiz"
 
 const container = document.getElementById("root");
 const root = createRoot(container);
