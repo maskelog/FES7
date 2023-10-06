68d806661134832d354c44472ca1c33f582332e5
maskelog <blitzmaske@gmail.com>
2023-10-06 13:46:58 +0900

    로그인버튼 추가

--- API_try/app/src/Components/LoginPage.jsx
+++ API_try/app/src/Components/LoginPage.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
+
 function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const inputEmail = (e)=>{
+    setEmail(e.target.value);
+  }
+  const inputPassword = (e)=>{
+    setPassword(e.target.value);
+  }
   return (
     <>
      <h1>로그인</h1>
      <section>
       <h2>이메일, 비밀번호 입력하는곳</h2>
       <from>
-        <input type="text" placeholder="이메일입력"/>
-        <input type="text" placeholder="비밀번호입력"/>
+        <input type="text" placeholder="이메일입력" onChange={inputEmail}/>
+        <input type="text" placeholder="비밀번호입력" onChange={inputPassword}/>
+        <button>로그인</button>
       </from>
      </section>
     </>
